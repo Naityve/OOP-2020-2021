8ac49b1a5e78d786b2d1f14632ddbd0a538d985f
Naityve <longinflorek@gmail.com>
2021-03-01 18:10:24 +0000

    update

--- java/src/ie/tudublin/StarMap.java
+++ java/src/ie/tudublin/StarMap.java
@@ -1,8 +1,49 @@
 package ie.tudublin;
 
+import java.util.ArrayList;
+
 import processing.core.PApplet;
+import processing.data.Table;
+import processing.data.TableRow;
 
 public class StarMap extends PApplet {
+    
+    ArrayList<Star> stars = new ArrayList<Star>();
+
+    void drawGrid()
+    {
+        float border = 0.05f * width;
+        for(int i = -5; i < 5; i++)
+        {
+            float x = map(i, -5, 5, border, width - border);
+            float y = map(1, -5, 5, border, height - border);
+            stroke(0, 0, 255);
+            line(x, border, x, height - border);
+            line(border, y, width - border, y);
+            fill(255);
+            text(1, x, border / 2);
+            text( 1, border / 2, y);
+        }
+    }
+
+    void printStars()
+    {
+        for(Star s: stars)
+        {
+            println(s);
+        }
+    }
+
+    void loadStars()
+    {
+        Table table = loadTable("HabHYG15ly.csv", "header");
+        for(TableRow row:table.rows())
+        {
+            Star s = new Star(row);
+            stars.add(s);
+        }
+    }
+
     public void settings() {
         size(500, 500);
     }
@@ -14,9 +55,12 @@ public class StarMap extends PApplet {
 
     public void setup() {
         colorMode(RGB);
+        loadStars();
+        printStars();
     }
 
     public void draw() {
         background(0);
+        drawGrid();
     }
 }
